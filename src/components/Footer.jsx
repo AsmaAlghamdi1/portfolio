07ff087cc2c5ffe7660b37ad0640732eb0318705
AsmaAlghamdi1 <Asmaa.sa1357@gmail.com>
2026-06-09 21:31:22 +0300

    fix footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,8 @@ function Footer() {
       <p>Designed & Developed by Asma Alghamdi</p>
 
       <div className="footer-icons">
-        <a href="#" aria-label="GitHub"><IoLogoGithub /></a>
-        <a href="#" aria-label="LinkedIn"><FaLinkedinIn /></a>
+        <a href="https://github.com/AsmaAlghamdi1" aria-label="GitHub"><IoLogoGithub /></a>
+        <a href="https://www.linkedin.com/in/asma-alghamdi-885a332b6?utm_source=share_via&utm_content=profile&utm_medium=member_ios" aria-label="LinkedIn"><FaLinkedinIn /></a>
         <a href="mailto:Asmaa.sa1357@gmail.com" aria-label="Email"><FaEnvelope /></a>
       </div>
     </footer>
